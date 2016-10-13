ba4848fe0cf2d504e8847d56d60673616eaaaa6a
Siressh <sireeshkodali1@gmail.com>
2016-10-13 09:28:31 +0530

    Remove pointingBox* as it seems to have a bug

--- main.cpp
+++ main.cpp
@@ -108,10 +108,6 @@ int main(int argc, char *argv[])
 		checkRows(sudoku_ans, sudoku_a, print_steps);
 		nakedSingle(sudoku_ans, sudoku_a, print_steps);
 		checkBox(sudoku_ans, sudoku_a, print_steps);
-		if (!sudoku_ans.changed)
-			pointingBoxColumns(sudoku_ans, print_steps);
-		if (!sudoku_ans.changed)
-			pointingBoxRows(sudoku_ans, print_steps);
 		if (!sudoku_ans.changed) 
 			boxLineReduceRow(sudoku_ans, print_steps);
 		if (!sudoku_ans.changed)
