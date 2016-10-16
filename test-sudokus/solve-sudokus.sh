57a62403409a8d043a803d0303760942074b79d7
Siressh <sireeshkodali1@gmail.com>
2016-10-16 21:52:43 +0530

    Add more test sudokus and create script that auto solves alll sudokus

---
+++ test-sudokus/solve-sudokus.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+program=~/Documents/School/11B/Computers/Sudoku-Solver/bin/Sudoku-solver
+testSudokus=~/Documents/School/11B/Computers/Sudoku-Solver/test-sudokus/test.sudoku
+
+echo "This program will solve all sudokus listed in test.sudoku using $program"
+
+while read in; do echo $in | $program -t; done < "$testSudokus"
