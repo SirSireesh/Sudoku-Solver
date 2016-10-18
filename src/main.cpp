ac5270e7fcb321188571e8315d4773da10032d69
Siressh <sireeshkodali1@gmail.com>
2016-10-18 19:05:54 +0530

    Correct code name

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
 					std::cout << "certain conditions; check license.txt for more details\n";
 					return 0;
 				case 'h':
-					std::cout << "Sudoku-solver : version 0.9.1 (Dharma)\n\n";
+					std::cout << "Sudoku-solver : version 0.9.1 (Doctor)\n\n";
 					std::cout << "usage : Sudoku-solver [arguments]\n";
 					std::cout << "Arguments:\n";
 					std::cout << " -a\t Print license info and exit\n";
@@ -61,7 +61,7 @@ int main(int argc, char *argv[])
 					}
 					break;
 				case 'v' :
-					std::cout << "Sudoku-solver : version 0.9.1 (Dharma)\n";
+					std::cout << "Sudoku-solver : version 0.9.1 (Doctor)\n";
 					std::cout << "Built with command: $ clang++ -I. Sudoku-solver.cpp main.cpp -o ./bin/Sudoku-solver -std=c++11 -O3 -march=native\n";
 					return 0;
 				case 's' :
